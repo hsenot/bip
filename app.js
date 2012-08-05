7c8ae72e544a911e46ef42c8ab90f6bcc91f57f2
Herve Senot <hsenot@gmail.com>
2012-08-05 13:55:59 +1000

    Transition WMS <-> WFS , building name display
    The layer is added to the map as WMS (when zoomed out) or as WFS+BBOX
    (when zoomed in)

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-var map,layer, geojson_layer,building_layer;
- 
+var map,layer,wfs_layer,building_layer,thresholdWFS=16;
+
+
+// Adding a proxy to use WFS features from another server
+OpenLayers.ProxyHost = "/geoexplorer/proxy?url=";
+
 OpenLayers.Feature.Vector.style['default']['strokeWidth'] = '2';
  
 function init(){
@@ -20,37 +24,80 @@ function init(){
                     ]
 	});
 	
+	
 	// OSM layer
-	layer = new OpenLayers.Layer.OSM( "Simple OSM Map");
+	layer = new OpenLayers.Layer.OSM("Simple OSM Map");
 	map.addLayer(layer);
 
-/*
-	// GeoJSON building layer
-	geojson_layer = new OpenLayers.Layer.Vector("GeoJSON", {
-		strategies: [new OpenLayers.Strategy.Fixed()],
-		protocol: new OpenLayers.Protocol.HTTP({
-				url: "data/GeoJSON/building.json",
-				format: new OpenLayers.Format.GeoJSON()
+	// Vector building layer
+	wfs_layer = new OpenLayers.Layer.Vector("WFS layer", {
+		strategies: [new OpenLayers.Strategy.BBOX()],
+		protocol: new OpenLayers.Protocol.WFS({
+			version: "1.1.0",
+			url: "http://basemap.pozi.com/geoserver/wfs",
+			featureType: "BUILDINGS",
+			featureNS: "http://www.pozi.com/building",
+			srsName: "EPSG:900913"
 		})
 	});
-	map.addLayer(geojson_layer);
-*/
 
+	// Image building layer
 	building_layer = new OpenLayers.Layer.WMS("Buildings", 
 		"http://basemap.pozi.com/geoserver/BUILDING/ows",
 		{layers: 'BUILDINGS',format:'image/png8',transparent:true},
 		{isBaseLayer: false,singleTile:true}
 	);
-	map.addLayer(building_layer);
 
-/*
+	// Managing switch between WFS and WMS, depending on zoom level
+	map.events.register("zoomend", this, function (e) {
+    	if (map.getZoom() >= thresholdWFS) {
+			// Close-up: we need WFS
+        	if (map.getLayerIndex(wfs_layer)==-1)
+			{map.addLayer(wfs_layer);wfs_layer.refresh({force:true});}
+
+			// Buildings are clickable! Inform the user
+			$("#current_building").html("Click on a building for more detail");
+
+			// And we can get rid of the WMS layer
+        	if (map.getLayerIndex(building_layer)>-1)
+ 			{map.removeLayer(building_layer);}
+			
+    	}
+    	else
+    	{
+    		// Removing the WFS layer
+        	if (map.getLayerIndex(wfs_layer)>-1)
+			{map.removeLayer(wfs_layer);}
+
+			// Display a message to say the buildings are not clickable
+			$("#current_building").html("Zoom in to be able to click on buildings");
+
+			// Bringing the WMS layer into play
+        	if (map.getLayerIndex(building_layer)==-1)
+			{map.addLayer(building_layer);}
+			
+    	}
+	});
+
+
 	// Function called on highlight
 	var report = function(e) {
+		var display_message;
 		OpenLayers.Console.log(e.type, e.feature.id);
+		if (e.feature.data.name)
+		{
+			display_message = "This is the '"+e.feature.data.name+"' building";
+		}
+		else
+		{
+			display_message = "This building has no name";
+		}
+		$("#current_building").html(display_message);
+		
 	};
 
 	// Highlight control            
-	var highlightCtrl = new OpenLayers.Control.SelectFeature(geojson_layer, {
+	var highlightCtrl = new OpenLayers.Control.SelectFeature(wfs_layer, {
 		hover: true,
 		highlightOnly: true,
 		renderIntent: "temporary",
@@ -62,7 +109,7 @@ function init(){
 	});
 	
 	// Select control
-	var selectCtrl = new OpenLayers.Control.SelectFeature(geojson_layer,
+	var selectCtrl = new OpenLayers.Control.SelectFeature(wfs_layer,
 		{clickout: true}
 	);
 
@@ -71,7 +118,6 @@ function init(){
 
 	highlightCtrl.activate();
 	selectCtrl.activate();
-*/
 
 	map.setCenter(
 		// Melbourne Uni
